6748a1016ce9560ff11447f7fe0733b0f728ded1
Arshdeep Singh <Arsh22sahni@gmail.com>
2020-08-06 05:21:48 +0530

    Create Patterns3.cpp

---
+++ Patterns3.cpp
@@ -0,0 +1,33 @@
+
+/*
+                   --------------------------
+                   |      CUSTOM EXAMPLE    |
+                   --------------------------
+                   | INPUT:     |   OUTPUT: |
+                   --------------------------
+                   |  4         |    1111   |
+                   |            |    2222   |
+                   |            |    3333   |
+                   |            |    4444   |
+                   --------------------------
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<i;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
