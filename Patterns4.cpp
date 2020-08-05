c731d204801562a4e2a22cc322c51046e2ae1a61
Arshdeep Singh <Arsh22sahni@gmail.com>
2020-08-06 05:28:46 +0530

    Create Patterns4.cpp

---
+++ Patterns4.cpp
@@ -0,0 +1,33 @@
+
+/*
+                   --------------------------
+                   |      CUSTOM EXAMPLE    |
+                   --------------------------
+                   | INPUT:     |   OUTPUT: |
+                   --------------------------
+                   |  4         |    4321   |
+                   |            |    4321   |
+                   |            |    4321   |
+                   |            |    4321   |
+                   --------------------------
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<(n-j+1);
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
