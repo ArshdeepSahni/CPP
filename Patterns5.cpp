e0abc13e9847b5b8e32c7c7cbc613a41a4612e67
Arshdeep Singh <Arsh22sahni@gmail.com>
2020-08-06 05:34:20 +0530

    Create Patterns5.cpp

---
+++ Patterns5.cpp
@@ -0,0 +1,32 @@
+/*
+                   --------------------------
+                   |      CUSTOM EXAMPLE    |
+                   --------------------------
+                   | INPUT:     |   OUTPUT: |
+                   --------------------------
+                   |  4         |    1234   |
+                   |            |    1234   |
+                   |            |    1234   |
+                   |            |    1234   |
+                   --------------------------
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            cout<<j;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
