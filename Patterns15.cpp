222585c86219a09619de43f8fb7b4330454a48da
Arshdeep Singh <Arsh22sahni@gmail.com>
2020-08-06 18:10:43 +0530

    Create Patterns15.cpp

---
+++ Patterns15.cpp
@@ -0,0 +1,43 @@
+  
+/*
+                   --------------------------
+                   |      CUSTOM EXAMPLE    |
+                   --------------------------
+                   | INPUT:     |   OUTPUT: |
+                   --------------------------
+                   |  4         |    ****   |
+                   |            |    ***    |
+                   |            |    **     |
+                   |            |    *      |
+                   --------------------------
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=(n-i))
+        {
+            cout<<' ';
+            j++;
+        }
+        int k=1;
+        while(k<=i){
+            cout<<k;
+            k++;
+        }
+        int p=k-2;
+        while(p>0){
+            cout<<p;
+            p--;
+        }
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
