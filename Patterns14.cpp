722e7046fe3d9a9a5104a3d46e05c19a3a0995e6
Arshdeep Singh <Arsh22sahni@gmail.com>
2020-08-22 22:25:53 +0530

    Revert " I updated your Patterns14.cpp as there was no need of spaces and an …"

--- Patterns14.cpp
+++ Patterns14.cpp
@@ -19,11 +19,16 @@ int main()
     int i=1;
     while(i<=n)
     {
-        int j=i;
-        while(j<=n)
+        int j=1;
+        while(j<=(n-i+1))
         {
             cout<<'*';
             j++;
+        }
+        int k=1;
+        while(k<i){
+            cout<<' ';
+            k++;
         }
         i++;
         cout<<endl;
